74ac7150bef56b80f4afd4ff6865dc53061adcbe
Georgiy <86735084+veomall@users.noreply.github.com>
2022-04-27 00:06:20 +0300

    py

---
+++ tetris.py
@@ -0,0 +1,135 @@
+import pygame
+from copy import deepcopy
+from random import choice, randrange
+
+
+W, H = 10, 20
+TILE = 35
+GAME_RES = W * TILE, H * TILE
+FPS = 60
+
+pygame.init()
+game_sc = pygame.display.set_mode(GAME_RES)
+clock = pygame.time.Clock()
+
+grid = [pygame.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(W) for y in range(H)]
+
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(-1, 1), (0, -1), (0, 0), (0, 1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figure_colors = [pygame.Color('skyblue'),
+                 pygame.Color('yellow'),
+                 pygame.Color('green'),
+                 pygame.Color('red'),
+                 pygame.Color('orange'),
+                 pygame.Color('pink'),
+                 pygame.Color('purple')]
+
+figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
+
+anim_count, anim_speed, anim_limit = 0, 60, 2000
+
+get_color = lambda: figure_colors[figures.index(figure)]
+figure = deepcopy(choice(figures))
+color = get_color()
+
+
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    if figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
+        return False
+    return True
+
+
+while True:
+    dx = 0
+    rotate = False
+    game_sc.fill(pygame.Color('black'))
+
+    # control
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                dx = -1
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                dx = 1
+            elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                anim_limit = 100
+            elif event.key == pygame.K_UP or event.key == pygame.K_w:
+                rotate = True
+
+    # move x
+    figure_old = deepcopy(figure)
+    for i in range(4):
+        figure[i].x += dx
+        if not check_borders():
+            figure = deepcopy(figure_old)
+            break
+
+    # move y
+    anim_count += anim_speed
+    if anim_count > anim_limit:
+        anim_count = 0
+        figure_old = deepcopy(figure)
+        for i in range(4):
+            figure[i].y += 1
+            if not check_borders():
+                for i in range(4):
+                    field[figure_old[i].y][figure_old[i].x] = color
+                figure = deepcopy(choice(figures))
+                color = get_color()
+                anim_limit = 2000
+                break
+
+    # rotate
+    center = figure[0]
+    figure_old = deepcopy(figure)
+    if rotate:
+        for i in range(4):
+            x = figure[i].y - center.y
+            y = figure[i].x - center.x
+            figure[i].x = center.x - x
+            figure[i].y = center.y + y
+            if not check_borders():
+                figure = deepcopy(figure_old)
+                break
+
+    # check lines
+    line = H - 1
+    for row in range(H - 1, -1, -1):
+        count = 0
+        for i in range(W):
+            if field[row][i]:
+                count += 1
+            field[line][i] = field[row][i]
+        if count < W:
+            line -= 1
+
+    # draw grid
+    [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, True) for i_rect in grid]
+
+    # draw figure
+    for i in range(4):
+        figure_rect.x = figure[i].x * TILE
+        figure_rect.y = figure[i].y * TILE
+        pygame.draw.rect(game_sc, color, figure_rect)
+
+    # draw field
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                figure_rect.x, figure_rect.y = x * TILE, y * TILE
+                pygame.draw.rect(game_sc, col, figure_rect)
+
+    pygame.display.flip()
+    clock.tick(FPS)
